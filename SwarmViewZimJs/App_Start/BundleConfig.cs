4f3b127713c75433d7920437041341f03c2fa00b
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2020-03-16 20:50:25 -0400

    First commit. Added solution files.

---
+++ SwarmViewZimJs/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SwarmViewZimJs
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/zimjs").Include(
+                      "~/Scripts/zimjs/createjs_doc_1.3.0.js",
+                      "~/Scripts/zimjs/zim_doc_10.8.0.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/view").Include(
+                      "~/Scripts/view/start.js"));
+        }
+    }
+}
---
+++ SwarmViewZimJs/Scripts/view/start.js
@@ -0,0 +1,54 @@
+﻿
+// SCALING OPTIONS
+// scaling can have values as follows with full being the default
+// "fit"    sets canvas and stage to dimensions and scales to fit inside window size
+// "outside"    sets canvas and stage to dimensions and scales to fit outside window size
+// "full"    sets stage to window size with no scaling
+// "tagID"    add canvas to HTML tag of ID - set to dimensions if provided - no scaling
+
+var scaling = "fit"; // this will resize to fit inside the screen dimensions
+var width = 1024;
+var height = 768;
+var color = light;
+var outerColor = darker;
+
+var frame = new Frame({ scaling, width, height, color, outerColor, retina: true });
+frame.on("ready", function () {
+
+    var stage = frame.stage;
+    var stageW = frame.width;
+    var stageH = frame.height;
+
+    frame.outerColor = '#444';
+    frame.color = '#ddd';
+
+    var bases = [];
+    var shadows = [];
+
+    var holderTile = new Container();
+    holderTile
+        .sca(2, 1)
+        .center();
+
+    var label = new Label("Hello").addTo(holderTile);
+
+    var label = new Label({
+        text: "CLICK",
+        size: 10,
+        font: "courier",
+        color: "white",
+        rollColor: "red",
+        fontOptions: "italic bold"
+    }).addTo(holderTile).ske(0, -45);
+
+    var win = new Window({
+        height: 300,
+        interactive: true,
+        padding: 0,
+        slideDamp: .2,
+        scrollBarDrag: true
+    }).pos(50, 20);
+
+    stage.update();
+
+});

3c7093bb7475b97b59d0f0e4de712d410186206c
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2020-03-17 20:00:04 -0400

    Added api endpoint controller to test return data from database.

---
+++ SwarmViewZimJs/Controllers/Api/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Repository;
+
+namespace SwarmViewZimJs.Controllers.Api
+{
+    public class SessionController : ApiController
+    {
+        public class SessionModel
+        {
+            public String Id { get; set; }
+            public String ProjectName { get; set; }
+            public String DeveloperName { get; set; }
+        }
+
+        // GET: api/Session
+        public IEnumerable<SessionModel> Get()
+        {
+            using (Entities context = new Entities())
+            {
+                return context.Sessions.Select(t => new SessionModel
+                {
+                    Id = t.Id.ToString(),
+                    ProjectName = t.ProjectName,
+                    DeveloperName = t.DeveloperName,
+                }).ToList();
+            }
+        }
+    }
+}
